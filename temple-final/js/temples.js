c04d049f8278248f059c7a6187d92e61ce4d1c53
HollyGammo <news4holly@gmail.com>
2022-07-09 16:45:39 -0400

    work

--- temple-final/js/temples.js
+++ temple-final/js/temples.js
@@ -1,28 +1,31 @@
 const requestURL = "json/templedata.json";
-const containers = document.querySelector(".temple-cards");
+const temples = document.querySelector(".temples");
 
-fetch(URLrequest)
-.then(function (response) {
-    return response.json();
-})
-.then(function (jsonObject) {
-    const information = jsonObject["temples"];
-    information.forEach(templeData);
-});
+async function findTemples() {
+    let response = await fetch(requestURL);
+    if (response.ok) {
+      let data = await response.json();
+      displayTemples(data);
+    } else {
+      throw Error(response.statusText);
+    }
+  }
+  
+  function displayTemples(data) {
+    data.temples.forEach(temple => {
 
-function templeData(temple) {
     let card = document.createElement("div");
     let imageSmall = document.createElement("img");
     let name = document.createElement("p");
-    let streetAddress = document.createElement("p");
-    let cityState = document.createElement("p");
+    let address = document.createElement("p");
+    let city = document.createElement("p");
     let phone = document.createElement("p");
     let email = document.createElement("p");
     let services = document.createElement("p");
     let history = document.createElement("p");
-    let ordinanceSch = document.createElement("p");
-    let sessionSch = document.createElement("p");
-    let closureSch = document.createElement("p");
+    let ordinances = document.createElement("p");
+    let sessions = document.createElement("p");
+    let closures = document.createElement("p");
     
     imageSmall.setAttribute('src', "images/" + temple.imageSmall);
     imageSmall.setAttribute('alt', `${temple.name}`);
@@ -38,5 +41,35 @@ function templeData(temple) {
     sessionSch.innerHTML = `${temple.sessionSch}`;
     closureSch.innerHTML = temple.closureSch;
 
-    containers.appendChild(card);
-}
+    button.classList.add("likeBtn");
+
+    if (temple.name == "Philadelphia Pennsylvania Temple") {
+        button.classList.add('phillyBtn')
+        button.setAttribute('onclick', 'phillyCount()');
+      } else if (temple.name == "Salt Lake Temple") {
+        button.classList.add('slcBtn')
+        button.setAttribute('onclick', 'slcCount()');
+      } else if (temple.name == "Nauvoo Illinois Temple") {
+        button.classList.add('NauvBtn')
+        button.setAttribute('onclick', 'NauvCount()');
+      } else if (temple.name == "Palmyra New York Temple") {
+        button.classList.add('palBtn')
+        button.setAttribute('onclick', 'palCount()');
+      }
+      
+      card.appendChild(imageSmall);
+      card.appendChild(button);
+      card.appendChild(name);
+      card.appendChild(address);
+      card.appendChild(city);
+      card.appendChild(phone);
+      card.appendChild(email);
+      card.appendChild(services);
+      card.appendChild(history);
+      card.appendChild(ordinances);
+      card.appendChild(sessions);
+      card.appendChild(closures);
+
+    temples.appendChild(card);
+ });
+} 
